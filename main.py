8435ab2ee535e2fbba557656ca3f4383ad686753
Cality <furrycality@gmail.com>
2023-04-10 19:54:28 -0500

    Bypass de nerfeo de ROBLOX

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def buy(json, itemid, productid):
 # Get collectible and product id for all the limiteds.
 Thread(target=get_x_token).start()
 
-print("UGCatcher by Furrycality™#1234\nWeb: https://rblx.furrycality.pw (Soon!)\nDiscord server: (Soon!)")
+print("UGCatcher by Furrycality™#1234 - Vr. 1.8\nWeb: https://rblx.furrycality.pw (Soon!)\nDiscord server: (Soon!)")
 while x_token == "":
     time.sleep(0.01)
 
@@ -100,7 +100,7 @@ while 1:
                            json={"items": [{"itemType": "Asset", "id": int(limited)}]},
                            headers={"x-csrf-token": x_token}, cookies={".ROBLOSECURITY": cookie}).json()["data"][0]
         except KeyError:
-            print("Ratelimit! Waiting for next minute to start / Ratelimited! Esperando el siguiente minuto para reiniciar")
+            print("Ratelimited! Esperando el siguiente minuto para reiniciar")
             time.sleep(60-int(datetime.datetime.now().second))
             continue
 
@@ -112,7 +112,7 @@ while 1:
             try:
                 productid = productid.json()[0]["collectibleProductId"]
             except:
-                print(f"Something went wrong whilst getting the product id / Algo ha salido mal obteniendo el id del articulo - Logs - {productid.text} - {productid.reason}")
+                print(f"Algo ha salido mal obteniendo el id del articulo - Logs - {productid.text} - {productid.reason}")
                 continue
 
             buy(info, info["collectibleItemId"], productid)
