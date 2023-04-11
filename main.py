b0605b50952bf89717a227c58ddb8f6e378d772d
Cality <furrycality@gmail.com>
2023-04-11 17:18:06 -0500

    2.1 -> 2.5 / Velocidad mejorada

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def buy(json, itemid, productid):
 # Get collectible and product id for all the limiteds.
 Thread(target=get_x_token).start()
 
-print("UGCatcher by Furrycality™#1234 - Vr. 2.1\nWeb: https://rblx.furrycality.pw (Soon!)\nAsistencia: https://discord.gg/WDbrnWpjpd")
+print("UGCatcher by Furrycality™#1234 - Vr. 2.5\nWeb: https://rblx.furrycality.pw (Soon!)\nAsistencia: https://discord.gg/WDbrnWpjpd")
 # os.system("pip install -update git+https://github.com/Furrycality/UGCatcher.git#egg=UGCatcher")
 os.system("start \"\" https://discord.gg/WDbrnWpjpd")
 while x_token == "":
